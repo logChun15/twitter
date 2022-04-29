69f889bc8aeb4c5b60b72a16d0ea7b8a094f5543
luochun <luochun15@gmail.com>
2022-04-29 15:37:41 +0800

    头部尾部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <a href="https://laravel-china.org/courses" target=_blank>
+      刻意练习，每日精进
+    </a>
+
+    <div class="float-end">
+      <a href="#" >关于</a>
+    </div>
+  </footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
 <html>
- <head>
- <title>@yield('title', 'twitter') - Laravel 入门教程</title>
- <link rel="stylesheet" href="{{ mix('css/app.css') }}">
- </head>
- <body>
- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
- <div class="container">
- <a class="navbar-brand" href="/">Twitter</a>
- <ul class="navbar-nav justify-content-end">
- <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
- <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
- </ul>
- </div>
- </nav>
- <div class="container">
- @yield('content')
- </div>
- </body>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        <hr>
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
