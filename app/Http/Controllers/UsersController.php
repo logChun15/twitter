c2e5a3fc50d26d89acaef2459bab1e1bb998e0ed
luochun <luochun15@gmail.com>
2022-04-29 18:00:10 +0800

    csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class UsersController extends Controller
 {
@@ -14,5 +15,15 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
