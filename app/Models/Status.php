f0ca4f2589b95569627d43e8c21777e3d3458d0c
luochun <luochun15@gmail.com>
2022-05-02 10:31:37 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['content'];
+    // 我们看到其中的关键信息 MassAssignmentException - 批量赋值异常，这是因为我们未在微博模型中
+    // 定义 fillable 属性，来指定在微博模型中可以进行正常更新的字段，Laravel 在尝试保护。解决的办
+    // 法很简单，在微博模型的 fillable 属性中允许更新微博的 content 字段即可。
     public function user() 
     {
         return $this->belongTo(User::class);
